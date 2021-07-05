112875d5087e7d9908894afbe67115e7ccf131e1
Veronica Blake/Noyce <vrex149@gmail.com>
2021-07-05 11:19:11 -0600

    auto upgrade?

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var totalFish = 0;
+
 let clickUpgrades = {
-    beverage: {
+    'beverage': {
         price: 2,
         quantity: 0,
         fishAdded: 1, 
         //this is how many are added to the total when the upgrade is run
     },
-    betterRod: {
+    'betterRod': {
         price: 3,
         quantity: 0,
         fishAdded: 2,
@@ -26,51 +26,52 @@ let autoUpgrades = {
     }
 }
 
-function click(){
-    totalFish ++
+var totalFish = 0;
+function addFish(){
+    totalFish++
+    for( let key in clickUpgrades){
+        totalFish += clickUpgrades[key].fishAdded * clickUpgrades[key].quantity
+    }
     console.log(totalFish)
-    document.getElementById('total-fish').innerHTML = `Total Fish:` + totalFish.toString()
     draw()
 }
 
-// function draw(key){
-//     for( key in upgrades){
-//         let upgrade = upgrades[key]
-//         //invoke update price and update modifier (they will be taking in upgrade)
-//         update(upgrade)
-//         update(modifier)
-//     }
-// }
+function draw(){
+    document.getElementById('total-fish').innerHTML = `Total Fish:` + totalFish.toString() 
+}
 
-// //applies the upgrade 
-// //2 click upgrades (beverage and reels) - these will increase how effective your clicks are.
-// //  bait will add 1 fish per click to the total
-// //  reel will add 2 fish per click to your total 
-// function buyClickUpgrade(){
-//     let upgrade = clickUpgrades[key].price
-//     if(totalFish >= upgrade){
-//         totalFish -= upgrade
-//         upgrades.quantity++ 
-//         modifier = ((upgrade.fishAdded)*(upgrades.quantity))
-//     }
-//     //take into account how many fish are needed to buy 
-//     //evaluate if totalFish is greater than or equal to price needed to buy
-//     //subtract the cost of the upgrade from totalFish
-//     //add the amountOfFishAddedFromUpgrade to the totalFishOnClick 
-//     //multiplier will go up
-//     //
-//     update()
-// }
+function buyClickUpgrade(key){
+    let upgrade = clickUpgrades[key].price
+    if(totalFish >= upgrade){
+        totalFish -= upgrade
+        clickUpgrades[key].quantity++ 
+        modifier = ((upgrade.fishAdded)*(clickUpgrades.quantity))
+    }
+    //INTERPOLATING IS THE BEEEEEST
+    document.getElementById(`${key}-total`).innerHTML = `x` + clickUpgrades[key].quantity.toString()
+    draw()
+}
 
-// function buyAutoUpgrade(){
-//     //take into account how many fish are needed to buy 
-//     //evaluate if totalFish is greater than or equal to 
-//     //subtract the cost of the upgrade from totalfish
-//     //fish will be added on the interval 
-// }
+///
 
-// // takes the priceMultiplier and multiplies the cost of the upgrade by that
-// function increaseUpgradeCost(upgradeCost){
+function buyAutoUpgrade(){
+    let upgrade = autoUpgrades[key].price
+    if(totalFish >= upgrade){
+        totalFish -= upgrade
+        autoUpgrades[key].quantity++ 
+        modifier = ((upgrade.fishAdded)*(autoUpgrades.quantity))
+    }
+    //INTERPOLATING IS THE BEEEEEST
+    document.getElementById(`${key}-total`).innerHTML = `x` + autoUpgrades[key].quantity.toString()
+    draw()
+    //take into account how many fish are needed to buy 
+    //evaluate if totalFish is greater than or equal to 
+    //subtract the cost of the upgrade from totalfish
+    //fish will be added on the interval 
+}
+
+// takes the priceMultiplier and multiplies the cost of the upgrade by that
+function increaseUpgradeCost(upgradeCost){
    
-// }
+}
 
