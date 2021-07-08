0aa15e94bcaad77b16bc20e13a515c8c57dcb9fe
Veronica Blake/Noyce <vrex149@gmail.com>
2021-07-08 10:37:37 -0600

    notes

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 let clickUpgrades = {
     'beverage': {
         price: 2,
@@ -27,21 +26,33 @@ let autoUpgrades = {
 }
 
 var totalFish = 0;
+
+//this function adds to the total fish, and for either of the click upgrades will 
+//add the additional fish multiplied by the quantity. Updates total fish count by calling draw.
 function addFish(){
     totalFish++
     for( let key in clickUpgrades){
         totalFish += clickUpgrades[key].fishAdded * clickUpgrades[key].quantity
     }
-    //  for( let key in autoUpgrades){
-    //     totalFish += autoUpgrades[key].fishAdded * autoUpgrades[key].quantity
-    // }
     draw()
+}
+// REVIEW there's got to be a way to put these into two functions but I'm sort of freaking rn
+//so I'll take what I can get 
+
+function addFishAuto(){
+    totalFish++
+    for(let key in autoUpgrades){
+        totalFish += autoUpgrades[key].fishAdded * autoUpgrades[key].quantity
+    }
+    console.log(totalFish)
 }
 
 function draw(){
     document.getElementById('total-fish').innerHTML = `Total Fish:` + totalFish.toString() 
 }
 
+
+//REVIEW What is "modiefier" doing??
 function buyClickUpgrade(key){
     let upgrade = clickUpgrades[key].price
     if(totalFish >= upgrade){
@@ -49,7 +60,6 @@ function buyClickUpgrade(key){
         clickUpgrades[key].quantity++ 
         modifier = ((upgrade.fishAdded)*(clickUpgrades.quantity))
     }
-    //INTERPOLATING IS THE BEEEEEST
     document.getElementById(`${key}-total`).innerHTML = `x` + clickUpgrades[key].quantity.toString()
     draw()
 }
